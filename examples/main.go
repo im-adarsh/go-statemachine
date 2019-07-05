6df4a72081c646d48fe52de98913d5230ad816ed
adarshkumar <adarsh.kumar@thecarousell.com>
2019-07-06 00:46:07 +0800

    init

--- examples/main.go
+++ examples/main.go
@@ -1,1 +1,48 @@
-package examples
+package main
+
+import (
+	"fmt"
+	fsm2 "github.com/looplab/fsm"
+)
+
+func main() {
+	fsm := fsm2.NewFSM(
+		"green",
+		fsm2.Events{
+			{Name: "warn", Src: []string{"green"}, Dst: "yellow"},
+
+		},
+		fsm2.Callbacks{
+			"before_warn": func(e *fsm2.Event) {
+				fmt.Println("before_warn")
+			},
+			"before_event": func(e *fsm2.Event) {
+				fmt.Println("before_event")
+			},
+			"leave_green": func(e *fsm2.Event) {
+				fmt.Println("leave_green")
+			},
+			"leave_state": func(e *fsm2.Event) {
+				fmt.Println("leave_state")
+			},
+			"enter_yellow": func(e *fsm2.Event) {
+				fmt.Println("enter_yellow")
+			},
+			"enter_state": func(e *fsm2.Event) {
+				fmt.Println("enter_state")
+			},
+			"after_warn": func(e *fsm2.Event) {
+				fmt.Println("after_warn")
+			},
+			"after_event": func(e *fsm2.Event) {
+				fmt.Println("after_event")
+			},
+		},
+	)
+	fmt.Println(fsm.Current())
+	err := fsm.Event("warn")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fsm.Current())
+}
